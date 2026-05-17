2dcfbde6c9ffc59374ad6fd14b3d45f12fafcaee
wjy1214 <wjy1065676928@gmail.com>
2026-05-17 20:28:37 +0800

    添加TWRP配置，包括屏幕尺寸和亮度设置

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,3 +99,11 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+
+# TWRP Configuration
+TW_SCREEN_HEIGHT := 2340
+TW_SCREEN_WIDTH := 1080
+
+TW_BRIGHTNESS_PATH := /sys/class/backlight/panel0-backlight/brightness
+TW_MAX_BRIGHTNESS := 2047
+TW_DEFAULT_BRIGHTNESS := 1024
