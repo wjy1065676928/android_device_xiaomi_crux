f5b701356d56e558525eb4f2c7e8d6ce851fc21c
wjy1065676928 <wjy1065676928@gmail.com>
2026-05-21 13:38:35 +0800

    1

--- twrp_crux.mk
+++ twrp_crux.mk
@@ -16,7 +16,7 @@ $(call inherit-product, device/xiaomi/crux/device.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := crux
-PRODUCT_NAME := twrp_cux
+PRODUCT_NAME := twrp_crux
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi9 Pro 5G
 PRODUCT_MANUFACTURER := Xiaomi
